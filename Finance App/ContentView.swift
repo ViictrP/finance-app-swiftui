998da9093e4d5d9c1308420ee6a6aee8a3875f21
viictrp <vpradodev@gmail.com>
2020-08-26 13:46:26 -0300

    Initial Commit

---
+++ Finance App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finance App
+//
+//  Created by Victor Prado on 26/08/20.
+//  Copyright © 2020 ViictrP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Finance AppTests/Finance_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Finance_AppTests.swift
+//  Finance AppTests
+//
+//  Created by Victor Prado on 26/08/20.
+//  Copyright © 2020 ViictrP. All rights reserved.
+//
+
+import XCTest
+@testable import Finance_App
+
+class Finance_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
